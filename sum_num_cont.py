bc34bce4656dfc14747999e676be3b44a21f583a
Ubuntu <ubuntu@ip-172-31-5-196.ec2.internal>
2022-01-14 05:33:45 +0000

    creaded a program that will ask the users for the amount of numbers and the numbes in order to find their sum

---
+++ sum_num_cont.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+
+# Created by: Melody Berhane
+# Created on: Jan. 12, 2022
+# This program asks the user how many numbers they would like
+# to add. It then uses a while loop to calculate and display
+# the sum of x numbers. A continue statement is also used.
+
+
+def main():
+    # initialize the loop counter and sum
+    loop_counter = 0
+    sum = 0
+    answer = ""
+
+    while True:
+        # get user number as a string
+        user_num_a_string = input("How many numbers would you like to add?: ")
+        print("")
+
+        try:
+            # converts the first user input to integer
+            user_num_a_int = int(user_num_a_string)
+
+            if user_num_a_int >= 0:
+                # calculate the sum of the entered numbers
+                while (loop_counter < user_num_a_int):
+                    # gets input from user
+                    user_num_b_string = input("Enter a whole number: ")
+
+                    try:
+                        # converts entered number from string to integer
+                        user_num_b_int = int(user_num_b_string)
+                        # joins the strings
+
+                        if user_num_b_int < 0:
+                            print("{} is < 0 and cannot be added"
+                                  .format(user_num_b_int))
+                            print("")
+                            continue
+
+                        print("{} added to the sum."
+                              .format(user_num_b_int))
+                        print("")
+                        sum = sum + user_num_b_int
+                        loop_counter = loop_counter + 1
+                        answer += str(user_num_b_int) + " + "
+                    except Exception:
+                        print("{} is not a number.". format(user_num_b_string))
+                        print("")
+                        continue
+                if loop_counter == user_num_a_int:
+                    print("{}0 = {} " .format(answer, sum))
+                    print("The sum is {}.".
+                          format(sum))
+                    break
+            else:
+                print("Please enter a whole number!")
+                print("")
+        except Exception:
+            print("{} is not a number.". format(user_num_a_string))
+            print("")
+
+
+if __name__ == "__main__":
+    main()
